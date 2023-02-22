3a286bb7b0c6ea60262ce4d6aa930fb0bb2ceab0
Yurubhillow <hillowyurub@gmail.com>
2023-02-22 00:38:24 -0800

    Task 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,34 +4,27 @@
  * @n: star number
  * Return: print de sucession of numbers to 98
  */
-void print_to_98(int n)
+void print_to_98(int n);
 {
-	while (n != 98)
+	int i, j;
+
+	if (n <= 98)
 	{
-		if (n > 9 && n <= 99)
+		for (i = n; i <= 98; i++)
 		{
-			_putchar((n / 10) + '0');
-			_putchar((n % 10) + '0');
+			if (i != 98)
+				printf("%d, ", i);
+			else if (i == 98)
+				printf("%d\n", i);
 		}
-		else if (n >= 100)
+	} else if (n >= 98)
+	{
+		for (j = n; j >= 98; j--)
 		{
-			_putchar((n / 100) + '0');
-			_putchar(((n / 10) % 10) + '0');
-			_putchar((n % 10) + '0');
+			if (j != 98)
+				printf("%d, ", j);
+			else if (j == 98)
+				printf("%d\n", j);
 		}
-		else
-			_putchar(n + '0');
-		_putchar(',');
-		_putchar(' ');
-
-	if (n > 98)
-
-		n--;
-	else
-
-		n++;
 	}
-	_putchar('9');
-	_putchar('8');
-	_putchar('\n');
 }
