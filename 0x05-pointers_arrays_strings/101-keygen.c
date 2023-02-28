a146b8f0d8cf41d2ea3b882c6a851899953ca503
Yurubhillow <hillowyurub@gmail.com>
2023-02-28 05:48:17 -0800

    Task 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -13,7 +13,7 @@ int main(void)
 	int pass[100];
 	int i, sum, n;
 
-	sum = 0
+	sum = 0;
 
 	srand(time(NULL));
 	for (i = 0; i < 100; i++)
@@ -25,7 +25,7 @@ int main(void)
 		{
 			n = 2772 - sum - '0';
 			sum += n;
-			putchar(n + '0')
+			putchar(n + '0');
 			break;
 		}
 	}
