dc81e338d52a28c8842b32675b36c56f4f570823
Yurubhillow <hillowyurub@gmail.com>
2023-03-02 02:06:07 -0800

    Task 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * string_toupper - Converts lowercase characters
+ * @s: First pointer parameter
+ * Return: a pointer containing uppercase character
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; *(s + i) != 0; i++)
+	{
+		if ((*(s + i) >= 97) && (*(s + i) <= 122))
+			*(s + i) -= 32;
+	}
+	return (s);
+}
